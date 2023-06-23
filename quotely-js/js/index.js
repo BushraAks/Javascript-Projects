85d8fcedb5324ba51cc5a69870f63f776f26e302
Bushra Aksoy <bbxsra17@gmail.com>
2023-06-23 20:32:52 +0300

    made changes

--- quotely-js/js/index.js
+++ quotely-js/js/index.js
@@ -18,22 +18,30 @@ for(let i=0; i<9; i++){
 }
 
 
-// printing all cards with quote details from the quote array
+// function for adding quote to card
+
 const cards = document.querySelector('.cards');
 
-quotes.forEach((quote)=>{
-    let card = document.createElement('div');
+function appendCards(img, content, writer){
+    const card = document.createElement('div');
+
     card.classList.add('card');
     card.classList.add('glassmorphism');
+
+    console.log(img, writer, content);
     card.innerHTML = 
         `
-        <img src="${quote.img}" alt="Image">
+        <img src="${img}" alt="Image">
         <div>
-            <p class="content">"${quote.content}"</p>
-            <h5 class="writer">${quote.writer} ~</h5>
+            <p class="content">"${content}"</p>
+            <h5 class="writer">${writer} ~</h5>
         </div>
         `
     cards.appendChild(card);
+}
+
+quotes.forEach((quote)=>{
+    appendCards(quote.img, quote.content, quote.writer);
 })
 
 
@@ -42,36 +50,13 @@ const addQuoteForm = document.querySelector('.add-quote-form');
 
 //  Adding listener for add quote button
 
-const imgFile = document.querySelector('#img-file').value;
-const writer = document.querySelector('#writer').value;
-const content = document.querySelector('#content').value;
+
 
 addQuoteButton.addEventListener('click', ()=>{
     console.log('clicked')
     addQuoteForm.style.visibility = 'visible';
-    quotes.push(new Quote(imgFile, writer, content))
 })
 
-// adding listener for add/ sumbit button
-
-const submitBtn = document.querySelector('#submit-btn');
-submitBtn.addEventListener('click', ()=>{
-    //
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.classList.add('glassmorphism');
-    card.innerHTML = 
-        `
-        <img src="${imgFile}" alt="Image">
-        <div>
-            <p class="content">"${content}"</p>
-            <h5 class="writer">${writer} ~</h5>
-        </div>
-        `
-    cards.appendChild(card);
-    //
-    addQuoteForm.style.visibility = 'hidden';
-})
 
 // listener for closing add quote form
 const faX = document.querySelector('.fa-x');
@@ -80,4 +65,20 @@ faX.addEventListener('click', ()=>{
 })
 
 
-// appending 
+// adding listener for add/ sumbit button
+
+const submitBtn = document.querySelector('#submit-btn');
+
+submitBtn.addEventListener('click', ()=>{
+    console.log('submit');
+    const imgFile = document.querySelector('#img-file').value;
+    console.log(imgFile);
+    const writer = document.querySelector('#writer').value;
+    const content = document.querySelector('#content').value;
+    quotes.push(new Quote(imgFile, writer, content));
+
+    appendCards(imgFile, content, writer)
+    addQuoteForm.style.visibility = 'hidden';
+})
+
+
