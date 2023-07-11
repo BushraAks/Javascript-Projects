d28d545dac2bc50f19cdaceba52313cd2275cf15
Bushra Aksoy <bbxsra17@gmail.com>
2023-07-11 10:07:23 +0300

    updated timer

--- timer/js/main.js
+++ timer/js/main.js
@@ -1,11 +1,47 @@
 const  startBtn = document.querySelector('#start-btn'),
        resetBtn = document.querySelector('#reset-btn'),
-       timeDisplay = document.querySelector('.time-display');
+       timeDisplay = document.querySelector('.time-display'),
+       dayNum = document.querySelector('.day-num'),
+       hourNum = document.querySelector('.hour-num'),
+       minNum = document.querySelector('.min-num'),
+       secNum = document.querySelector('.sec-num');
 
 let stoped = true;
-let hrs = 0;
-let mins = 0;
-let secs = 0;
+const second = 1000; // 1 second = 1000 milliseconds
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const nums = document.querySelectorAll('.num');
+
+
+function countDown(){
+    let dateTo = new Date('August 1, 2023 00:00:00').getTime();
+    let dateNow = new Date().getTime();
+    let gap = dateTo - dateNow;
+
+    const dayTxt = Math.floor(gap / day);
+    const hourTxt = Math.floor((gap % day) / hour);
+    const minTxt = Math.floor((gap % hour) / minute);
+    const secTxt = Math.floor((gap % minute) / second);
+
+    if (dayTxt == '0' && hourTxt == '0' && minTxt == '0' && secTxt <= 30) {
+        nums.forEach((num) => {
+            num.style.color = 'red';
+        })
+    }
+    else {
+        nums.forEach((num) => {
+            num.style.color = '#40c437';
+        })
+    }
+    dayNum.innerText =  addZero(dayTxt);
+    hourNum.innerText = addZero(hourTxt);
+    minNum.innerText = addZero(minTxt);
+    secNum.innerText = addZero(secTxt);
+
+}
+
+setInterval(countDown, 75);
 
 
 // start button click
@@ -31,6 +67,7 @@ resetBtn.addEventListener('click', () => {
 })
 
 
+// util functions
 function addRemoveClass(elements, newClass, oldClass){
     if (Array.isArray(elements)){
         elements.forEach((element) => {
@@ -42,4 +79,8 @@ function addRemoveClass(elements, newClass, oldClass){
         elements.classList.add(newClass);
         elements.classList.remove(oldClass);
     }
+}
+
+function addZero(digit) {
+    return (('0') + digit).length > 2 ? digit : '0' + digit;
 }
